598557d4cc51d3a52a66e556b770224694a36ba7
martytheparty <marty@ilikeemail.com>
2018-02-24 15:47:45 -0500

    Adds Controls (RxJs)

--- cjgammon/02basic3d/app.js
+++ cjgammon/02basic3d/app.js
@@ -1,4 +1,26 @@
-var THREE = require('three');
+const THREE = require('three');
+const Rx = require('rxjs');
+const zoomInput = document.getElementById('zoomInput');
+const speedInput = document.getElementById('speedInput');
+let currentSpeed = .01;
+let cameraPosition = -1000;
+speedInput.value = currentSpeed*100;
+zoomInput.value = cameraPosition * -1 / 100;
+
+Rx.Observable.fromEvent(zoomInput, 'change').subscribe(
+  function(event) {
+    cameraPosition = event.target.value * -100;
+  }
+);
+
+Rx.Observable.fromEvent(speedInput, 'change').subscribe(
+  function(event) {
+    currentSpeed = event.target.value/100;
+  }
+);
+
+
+console.log(Rx);
 
 var renderer = new THREE.WebGLRenderer(
   {
@@ -9,7 +31,7 @@ var renderer = new THREE.WebGLRenderer(
 
 renderer.setSize(window.innerWidth, window.innerHeight);
 
-var camera = new THREE.PerspectiveCamera(35, window.innerWidth/window.innerHeight, 0.1, 3000);
+var camera = new THREE.PerspectiveCamera(35, window.innerWidth/window.innerHeight, 0.1, 10000);
 // camera.position(0,0,0);
 
 
@@ -25,12 +47,13 @@ scene.add(light1);
 var geometry = new THREE.CubeGeometry(100, 100, 100);
 var material = new THREE.MeshLambertMaterial({color:0xffcccc});
 var mesh = new THREE.Mesh(geometry, material);
-mesh.position.set(0,0,-1000);
+mesh.position.set(0,0,cameraPosition);
 scene.add(mesh);
 
 function render() {
-  mesh.rotation.x += 0.01;
-  mesh.rotation.y += 0.01;
+  mesh.rotation.x += currentSpeed;
+  mesh.rotation.y += currentSpeed;
+  mesh.position.set(0,0,cameraPosition);
   renderer.render(scene, camera);
   requestAnimationFrame(render);
 }
